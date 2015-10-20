d99fd3d3c1d82ad862681edd9a3a812eb210b666
Fatma Ocal <contactfatma@gmail.com>
2015-10-20 14:47:06 -0700

    Include the rules for leap year

--- week-4/leap-years/my_solution.rb
+++ week-4/leap-years/my_solution.rb
@@ -2,5 +2,16 @@
 
 # I worked on this challenge [by myself, with: ].
 
+#Every four years is a leap_year.
+#2016, 2020 are leap years.
+#check input is divisible by four.
+#use modulus function with return of 0 when it is divided by 4.
+# year = 2000 is a leap year, because it is divisible by 400.
+# year = 1980 is not a lep year, because it is not divisible by 400, but divisible by 100.
 
 # Your Solution Below
+def leap_year?(year)
+
+  year % 4 == 0 && (year % 100 !=0 || year % 400 ==0 )
+
+end
