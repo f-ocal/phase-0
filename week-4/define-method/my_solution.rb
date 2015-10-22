bea7005b394e45ecd948aa5a6070fbe192293f53
Fatma Ocal <contactfatma@gmail.com>
2015-10-21 23:40:24 -0700

    Insert largest and shortest integer

--- week-4/define-method/my_solution.rb
+++ week-4/define-method/my_solution.rb
@@ -1,18 +1,9 @@
 # Define an Empty Method
-def do_something(first_name, last_name, age)
 
-end
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge myself.
 
 
 # Your Solution Below
+def do_something(first_name, last_name, age)
 
-
-# Release 6: Reflect
-#
-# How do you define a local variable?
-# How do you define a method?
-# What is the difference between a local variable and a method?
-# How do you run a ruby program from the command line?
-# How do you run an RSpec file from the command line?
-# What was confusing about this material? What made sense?
+end
--- week-4/good-guess/my_solution.rb
+++ week-4/good-guess/my_solution.rb
@@ -2,6 +2,6 @@
 
 # I worked on this challenge Abe and Brian.
 def good_guess?(num)
-num == 42
+  num == 42
 end
 # Your Solution Below
