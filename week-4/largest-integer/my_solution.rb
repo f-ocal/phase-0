edfeefa17af4533fe24b6faa2e0b741aaf9b79cb
Fatma Ocal <contactfatma@gmail.com>
2015-10-23 00:28:56 -0700

    Insert code for largest

--- week-4/largest-integer/my_solution.rb
+++ week-4/largest-integer/my_solution.rb
@@ -12,6 +12,21 @@
 # If +list_of_nums+ is empty the method should return nil
 
 # Your Solution Below
-def largest_integer(list_of_nums)
-  # Your code goes here!
+def largest_integer(list_of_numbers)
+  # Return if the array doesn't have any element.
+  return nil if list_of_numbers.length == 0
+  # Define the variable to the first element of the array.
+  largest_number = list_of_numbers[0]
+  # Take the first number!! and store inside defined variable.
+  list_of_numbers.each do |number|
+    # Go ahead and take the next one
+    # Compare that one with the stored number in the variable.
+    if largest_number < number
+      # Whichever is larger, keep that number inside the variable.
+      largest_number = number
+    end
+    # Continue this process until the array is empty.
+  end
+  # Return the largest number.
+  largest_number
 end
