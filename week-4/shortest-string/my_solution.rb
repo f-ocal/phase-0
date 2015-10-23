626c585d98ccfa965d0672e3fac1977c1de25790
Fatma Ocal <contactfatma@gmail.com>
2015-10-23 12:42:07 -0700

    Include code for longest string

--- week-4/shortest-string/my_solution.rb
+++ week-4/shortest-string/my_solution.rb
@@ -15,26 +15,27 @@
 
 #Your Solution Below
 
-# def shortest_string(list_of_words)
-# # Return to nil if the array is zero.
-#   return nil if list_of_words.length == 0
-# # Define the variable to the first element of the array.
-#   shortest_number=list_of_words[0]
-# # Take the next element from the array, compare its length with the stored element's length.
-#     list_of_words.each do |word|
-#       if word.length  < shortest_number.length
-#   # If its shorter, replace it with the variable. Otherwise, do not keep that element.
-#         shortest_number=word
-#       end
-# # Continue this until the array is empty!
-#     end
-#   shortest_number
-# # Once the array is empty than return the shortest number.
-# end
-
-#Refactored Solution
-
 def shortest_string(list_of_words)
-  list_of_words.min { |x,y| x.length <=> y.length }
+# Return to nil if the array is zero.
+  return nil if list_of_words.length == 0
+  # Define the variable to the first element of the array.
+  shortest_number=list_of_words[0]
+  # Take the next element from the array, compare its length with the stored element's length.
+    list_of_words.each do |word|
+      if word.length  < shortest_number.length
+        # If its shorter, replace it with the variable. Otherwise, do not keep that element.
+        shortest_number=word
+      end
+      # Continue this until the array is empty!
+    end
+  shortest_number
+# Once the array is empty than return the shortest number.
 end
-puts shortest_string(["cat", "fatma"])
+
+puts shortest_string(["5","4","1","fatma"])
+#
+# #Refactored Solution
+#
+# def shortest_string(list_of_words)
+#   list_of_words.min { |x,y| x.length <=> y.length }
+# end
