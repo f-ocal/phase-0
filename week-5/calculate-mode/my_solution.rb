a6244a54f8283e5c31c2680910de97a32c00116d
Fatma Ocal <contactfatma@gmail.com>
2015-10-27 16:05:39 -0700

    Update pairing challenges

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -1,24 +1,27 @@
 # Calculate the mode Pairing Challenge
 
-# I worked on this challenge [by myself, with: ]
-
-# I spent [] hours on this challenge.
-
-# Complete each step below according to the challenge directions and
-# include it in this file. Also make sure everything that isn't code
-# is commented.
-
-
-
-# 0. Pseudocode
+# I worked on this challenge with Abe, Brian and Fatma.
+# I spent 2 hours on this challenge.
 
 # What is the input?
+# an array contains integers.
 # What is the output? (i.e. What should the code return?)
+# Output is the key of the hash which has the highest value.
 # What are the steps needed to solve the problem?
-
+# Please see below Pseudocode
 
 # 1. Initial Solution
 # 0. Pseudocode
+
+# Define a new empty hash
+# iterate over the array
+# check if array's value is already a key in the hash
+# if so, append to the corresponding array
+# if not, create new key and corresponding array
+# define a new variable to give the highest value from the hash
+# use each method to iterate over hash
+# if highest value from the hash is equals to the value from the old hash, than
+# push key to new array of all keys that are equal to longest array length
 
 def mode(array)
   new_hash = {}
@@ -36,7 +39,7 @@ def mode(array)
       result.push(x)
     end
   }
-  return result
+  # return result
   puts result
 end
 mode([1,2,3,4,5,4,2,2])
@@ -46,3 +49,7 @@ mode([1,2,3,4,5,4,2,2])
 
 
 # 4. Reflection
+# Which data structure did you and your pair decide to implement and why?
+# Were you more successful breaking this problem down into implementable pseudocode than the last with a pair?
+# What issues/successes did you run into when translating your pseudocode to code?
+# What methods did you use to iterate through the content? Did you find any good ones when you were refactoring? Were they difficult to implement?
