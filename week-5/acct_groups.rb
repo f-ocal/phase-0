b3aab25df12a1529045a0b26018d0fac4fc1fa1d
Fatma Ocal <contactfatma@gmail.com>
2015-10-30 23:25:42 -0700

    Include accountability code

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -1,33 +1,43 @@
 # Creating accountability groups into fives, or fours and threes.
+# Pseudocode
+# Input : An array of list of students.
+# Output : An array of arrays that contains student names in groups of 3-5 with preference of larger groups.
+# group into fives.
+# check last group size. If last group size is:
+# 1 -> take last two groups and make them 3,3
+#      Take 2nd to last group, combine with last group, slice into groups of 3
+# 2 -> take last two groups and make them 4,3
+#      Take 2nd to last group, combine with last group, slice into groups of 4
+# 3 -> no action
+# 4 -> no action
+# 5 -> no action
 
 def slice_into_groups(elements, slice_size)
   result = []
-  elements.each_slice(slice_size) do |element_group|
-      result << element_group
-  end
+  elements.each_slice(slice_size) { |element_group| result << element_group }
   result
+end
+
+def combine_last_two_groups!(result)
+  result.slice!(-2,2).flatten
 end
 
 def group_people(students)
-  # group into fives.
-  # check last group size. If last group size is:
-  # 1 -> take last two groups and make them 3,3
-  #      Take 2nd to last group, combine with last group, slice into groups of 3
-  # 2 -> take last two groups and make them 4,3
-  #      Take 2nd to last group, combine with last group, slice into groups of 4
-  # 3 -> no action
-  # 4 -> no action
-  # 5 -> no action
-
+  return nil if students.length < 3
   result = slice_into_groups(students, 5)
   if result.last.length == 2
-    last_two_groups = result.slice!(-2,2)
-    result += slice_into_groups(last_two_groups.flatten, 4)
-  end
-
-  if result.last.length == 1
-    last_two_groups = result.slice!(-2,2)
-    result += slice_into_groups(last_two_groups.flatten, 3)
+    result += slice_into_groups(combine_last_two_groups!(result), 4)
+  elsif result.last.length == 1
+    result += slice_into_groups(combine_last_two_groups!(result), 3)
   end
   result
 end
+
+# Release 6: Reflect
+# Create a commented-out reflection section in your acct_groups.rb file to answer the following questions in:
+# 
+# What was the most interesting and most difficult part of this challenge?
+# Do you feel you are improving in your ability to write pseudocode and break the problem down?
+# Was your approach for automating this task a good solution? What could have made it even better?
+# What data structure did you decide to store the accountability groups in and why?
+# What did you learn in the process of refactoring your initial solution? Did you learn any new Ruby methods?
--- week-5/acct_groups_spec.rb
+++ week-5/acct_groups_spec.rb
@@ -1,26 +1,40 @@
 require_relative "acct_groups"
 
 describe 'accountability group method' do
-  let(:students_12) { ["Fatma Ocal", "Jim fung", "James", "Fatos", "1","Jenny", "Hung", "Man", "2", "H1", "H2", "3"] }
-  let(:students_11) { ["Fatma Ocal", "Jim fung", "James", "Fatos", "1","Jenny", "Hung", "Man", "2", "H1", "H2"] }
-  let(:students_10) { ["Fatma Ocal", "Jim fung", "James", "Fatos", "1","Jenny", "Hung", "Man", "2", "H1"] }
+  let(:students_12) { ["Fatma", "Yasemin", "Burcu", "Zahide", "Nezahat","Bekir", "Kerem", "Ali", "Jim", "Jenny", "Alice", "Ayca"] }
+  let(:students_11) { ["Fatma", "Yasemin", "Burcu", "Zahide", "Nezahat","Bekir", "Kerem", "Ali", "Jim", "Jenny", "Alice"] }
+  let(:students_10) { ["Fatma", "Yasemin", "Burcu", "Zahide", "Nezahat","Bekir", "Kerem", "Ali", "Jim", "Jenny"] }
+  let(:students_1) { ["Fatma"] }
+  let(:students_2) { ["Fatma","Yasemin"] }
+  let(:students_3) {["Fatma", "Yasemin", "Burcu"]}
 
   it "separates 12 students into groups of 5 and 4 and 3" do
-  #  expect(group_people(students)).to eq group_1: ["Fatma Ocal", "Jim fung", "James", "Fatos"],
-  #                                       group_2: ["Jenny", "Hung", "Man"]
-  expect(group_people(students_12)).to eq [["Fatma Ocal", "Jim fung", "James", "Fatos", "1"],
-                                       ["Jenny", "Hung", "Man", "2"],
-                                       ["H1", "H2", "3"]]
+    expect(group_people(students_12)).to eq [["Fatma", "Yasemin", "Burcu", "Zahide", "Nezahat"],
+    ["Bekir", "Kerem", "Ali", "Jim"],
+    ["Jenny", "Alice", "Ayca"]]
   end
 
   it "separates 11 students into groups of 5 and 3 and 3" do
-  expect(group_people(students_11)).to eq [["Fatma Ocal", "Jim fung", "James", "Fatos", "1"],
-                                       ["Jenny", "Hung", "Man"],
-                                       ["2","H1", "H2"]]
+    expect(group_people(students_11)).to eq [["Fatma", "Yasemin", "Burcu", "Zahide", "Nezahat"],
+    ["Bekir", "Kerem", "Ali"],
+    ["Jim", "Jenny", "Alice"]]
   end
 
   it "separates 10 students into groups of 5 and 5" do
-  expect(group_people(students_10)).to eq [["Fatma Ocal", "Jim fung", "James", "Fatos", "1"],
-                                           ["Jenny", "Hung", "Man", "2","H1"]]
+    expect(group_people(students_10)).to eq [["Fatma", "Yasemin", "Burcu", "Zahide", "Nezahat"],
+    ["Bekir", "Kerem", "Ali", "Jim", "Jenny"]]
   end
+
+  it "return nil if it is only one students" do
+    expect(group_people(students_1)).to eq nil
+  end
+
+  it "return nil if it is only two students" do
+    expect(group_people(students_2)).to eq nil
+  end
+
+  it "separates 3 students into group of three" do
+    expect(group_people(students_3)).to eq [["Fatma", "Yasemin", "Burcu"]]
+  end
+
 end
