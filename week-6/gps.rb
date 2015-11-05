c0ccb180961d79d54a36d57679499f2a18ed8e5d
Fatma Ocal <contactfatma@gmail.com>
2015-11-04 21:31:24 -0800

    Include gps :

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -7,7 +7,7 @@
 # 1) Fatma Ocal
 # 2) Alan
 
-# We spent[#] hours on this challenge.
+# We spent 1.5 hours on this challenge.
 
 # Bakery Serving Size portion calculator.
 
@@ -16,20 +16,20 @@ def serving_size_calc(item_to_make, order_quantity)
   # library is the _____________.
   # ingredients
   # desert
-#   ingredints_per_desert
+  #   ingredints_per_desert
 
   ingredients_per_desert = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-#   error_counter = 3
+  #   error_counter = 3
 
   # Figure out if the bakery can make the requested food.
   # aka does the list of food INCLUDE the item_to_make.
   # Hash#include?
   # Are there any built in methods that can do this for you?
-#   ingredints_per_desert.each do |k, v|
-#     if ingredints_per_desert[food] != ingredints_per_desert[item_to_make]
-#       error_counter += -1
-#     end
-#   end
+  #   ingredints_per_desert.each do |k, v|
+  #     if ingredints_per_desert[food] != ingredints_per_desert[item_to_make]
+  #       error_counter += -1
+  #     end
+  #   end
 
   can_make_food = ingredients_per_desert.include?(item_to_make)
 
@@ -48,7 +48,6 @@ def serving_size_calc(item_to_make, order_quantity)
   # Find the value at key = item_to_make
   # So if we pass in "cookie" => [1]
   #                  "cake"   => [5]
-
 
   # Values at returns an ARRAY of all values that match a given key.
   # However, since there can only be one of each key in the hash...
@@ -69,14 +68,13 @@ def serving_size_calc(item_to_make, order_quantity)
   else
     "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{leftover_ingredients} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
   end
-#   case serving_size_mod
-#   when 0
-#     return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
-#   else
-#     return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
-#   end
+  #   case serving_size_mod
+  #   when 0
+  #     return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
+  #   else
+  #     return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+  #   end
 end
-
 
 # calls various items with quantities
 
@@ -88,4 +86,39 @@ p serving_size_calc("cookie", 1)
 p serving_size_calc("cookie", 10)
 #p serving_size_calc("THIS IS AN ERROR", 5)
 
+# Refactored Solution
+# def serving_size_calc(item_to_make, order_quantity)
+#   ingredients_per_desert = {"cookie" => 1, "cake" =>  5, "pie" => 7}
+#   can_make_food = ingredients_per_desert.include?(item_to_make)
+#   unless can_make_food #error_counter > 0
+#     raise ArgumentError.new("#{item_to_make} is not a valid input")
+#   end
+#   serving_size = ingredients_per_desert[item_to_make]
+#   leftover_ingredients = order_quantity % serving_size
+#   amount = ""
+#   quantity = 0
+#
+#   ingredients_per_desert.each do |k, v|
+#     if(leftover_ingredients % v == 0)
+#       amount = k
+#       quantity = leftover_ingredients / v
+#     end
+#   end
+#   leftover_ingredients = order_quantity % serving_size
+#   if leftover_ingredients == 0
+#     return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
+#   else
+#     "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{leftover_ingredients} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+#   end
+# end
+#
+# p serving_size_calc("pie", 7)
+# p serving_size_calc("pie", 8)
+# p serving_size_calc("cake", 5)
+# p serving_size_calc("cake", 7)
+# p serving_size_calc("cookie", 1)
+# p serving_size_calc("cookie", 10)
+# #p serving_size_calc("THIS IS AN ERROR", 5)
+#
+# #  Reflection
 #  Reflection
