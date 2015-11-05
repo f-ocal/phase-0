ebd3cf26e56f13ccc12231c7541b5ae971c449c2
Fatma Ocal <contactfatma@gmail.com>
2015-11-04 21:42:32 -0800

    Include creadit card solution

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -28,42 +28,41 @@
 # Don't forget to check on initialization for a card length
 # of exactly 16 digits
 
-# (array.dup.to_s.gsub(" ", "").length)
+# (array.dup.to_s.gsub(" ", "").length) to remove the whitespace if input includes.
 
-#
-# class CreditCard
-#
-#   def initialize(input)
-#     raise ArgumentError.new('The card is not valid') unless (input.to_s.length == 16 ) && (input.is_a? Integer)
-#     @card = input
-#     @accumulator = 0
-#   end
-#
-#   def double_even_index
-#     array = @card.to_s.split(//)
-#     array.map! {|character| character.to_i }
-#     (0..15).each { |index| (array[index] =(array[index] * 2)) if index.even? }
-#     return array
-#   end
-#
-#   def sum_digits(array)
-#     array.each do |digit|
-#       if (digit < 10)
-#         @accumulator += digit
-#       else
-#         digit.to_s.split(//).each {|x| @accumulator += x.to_i }
-#       end
-#     end
-#   end
-#
-#   def check_card
-#     sum_digits(double_even_index)
-#     @accumulator % 10 == 0
-#   end
-# end
+class CreditCard
 
-# cc=CreditCard.new(1234567891234567)
-# p cc.check_card
+  def initialize(input)
+    raise ArgumentError.new('The card is not valid') unless (input.to_s.length == 16 ) && (input.is_a? Integer)
+    @card = input
+    @accumulator = 0
+  end
+
+  def double_even_index
+    array = @card.to_s.split(//)
+    array.map! {|character| character.to_i }
+    (0..15).each { |index| (array[index] =(array[index] * 2)) if index.even? }
+    return array
+  end
+
+  def sum_digits(array)
+    array.each do |digit|
+      if (digit < 10)
+        @accumulator += digit
+      else
+        digit.to_s.split(//).each {|x| @accumulator += x.to_i }
+      end
+    end
+  end
+
+  def check_card
+    sum_digits(double_even_index)
+    @accumulator % 10 == 0
+  end
+end
+
+cc=CreditCard.new(1234567891234567)
+p cc.check_card
 
 
 # Refactored Solution
@@ -103,10 +102,7 @@ class CreditCard
   end
 end
 
-
-
-
-
-
-
 # Reflection
+# What was the most difficult part of this challenge for you and your pair?
+# What new methods did you find to help you when you refactored?
+# What concepts or learnings were you able to solidify in this challenge?
