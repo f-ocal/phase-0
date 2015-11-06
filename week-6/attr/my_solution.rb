1ba81a497ee60c84e121411f8bdd92e4ba6187d1
Fatma Ocal <contactfatma@gmail.com>
2015-11-06 02:13:28 -0800

    Update new files

---
+++ week-6/attr/my_solution.rb
@@ -0,0 +1,30 @@
+
+#Attr Methods
+
+# I worked on this challenge [by myself, with:]
+
+# I spent [#] hours on this challenge.
+
+# Pseudocode
+
+# Input:
+# Output:
+# Steps:
+
+class NameData
+
+end
+
+
+class Greetings
+
+end
+
+
+
+# Reflection
+# What is a reader method?
+# What is a writer method?
+# What do the attr methods do for you?
+# Should you always use an accessor to cover your bases? Why or why not?
+# What is confusing to you about these methods?
