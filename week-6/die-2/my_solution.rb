118f12d01379ce011d549105ddd0cca1c91be7da
Fatma Ocal <contactfatma@gmail.com>
2015-11-05 18:19:06 -0800

    Include reflection questions

--- week-6/die-2/my_solution.rb
+++ week-6/die-2/my_solution.rb
@@ -34,5 +34,9 @@ end
 
 
 
-
 # Reflection
+#
+# What were the main differences between this die class and the last one you created in terms of implementation? Did you need to change much logic to get this to work?
+# What does this exercise teach you about making code that is easily changeable or modifiable?
+# What new methods did you learn when working on this challenge, if any?
+# What concepts about classes were you able to solidify in this challenge?
