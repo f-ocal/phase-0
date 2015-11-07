7bbdb130bdf6fc2a7efccac12d41769657f50378
Fatma Ocal <contactfatma@gmail.com>
2015-11-07 15:43:38 -0800

    Include release 2 reflection

--- week-6/attr/my_solution.rb
+++ week-6/attr/my_solution.rb
@@ -24,7 +24,7 @@ end
 =begin
 
 What are these methods doing?
-The methods are creating an instance and behaviours of Profile class, the instance is called instance_of_profile. The object is initialized with Kim's profile details : name, last name, age and occupation. The methods are defined to change and update the instance variables (their states name, age and occupation) with instance_of_profile.change_my_xxxxx so does the object's states are updated. 
+The methods are creating an instance and behaviours of Profile class, the instance is called instance_of_profile. The object is initialized with Kim's profile details : name, last name, age and occupation. The methods are defined to change and update the instance variables (their states name, age and occupation) with instance_of_profile.change_my_xxxxx so does the object's states are updated.
 
 How are they modifying or returning the value of instance variables?
 The methods are modified and changed the value of instance variables outside of the class by calling them with attributes meaning that by calling class. method. When we write the instance variable value, we use writer methods. Instead of giving arguments between parentheses, methods expect the new value after `=` notation such as
@@ -35,9 +35,17 @@ instance_of_profile.change_my_age = 28 # happy birthday! you aged one year
 # Reflection - Release 2: Identify the Changes
 =begin
 What changed between the last release and this release?
+From the output perspective, nothing really changed. When we run the code, it produces the same profile information for Kim and Taylor.
+
 What was replaced?
 Is this code simpler than the last?
+We replaced def what_is_age; end method with attr_reader :age both are the same things. It is definitely simplier. 
 
+# def what_is_age
+#   @age
+# end
+
+# attr_reader :age
 
 =end
 
