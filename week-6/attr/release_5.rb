4e551784e753dd3fc4156ba8d0160db404d0efe4
Fatma Ocal <contactfatma@gmail.com>
2015-11-07 18:03:06 -0800

    Include release 5

--- week-6/attr/release_5.rb
+++ week-6/attr/release_5.rb
@@ -4,42 +4,58 @@
 
 # Your Code Here:
 
-class Profile
+# class Profile
 # first let's finish refactoring this code
 # note that there is a way to add all of the variables like :age on one line, how would we do that?
-  attr_accessor :age
+#   attr_accessor :age
+#   attr_accessor :name
+#   attr_accessor :occupation
+#
+#   def initialize
+#     @age = 27
+#     @name = "Kim"
+#     @occupation = "Cartographer"
+#   end
+#
+#   def print_info
+#     puts
+#     puts "age: #{@age}"
+#     puts
+#     puts "name: #{@name}"
+#     puts
+#     puts "occupation: #{@occupation}"
+#     puts
+#   end
+# end
+
+####################
+
+class NameData
+
+  attr_reader :name
 
   def initialize
-    @age = 27
-    @name = "Kim"
-    @occupation = "Cartographer"
+    @name = 'Fatma'
   end
-
-  def print_info
-    puts
-    puts "age: #{@age}"
-    puts
-    puts "name: #{@name}"
-    puts
-    puts "occupation: #{@occupation}"
-    puts
-  end
-
-  def what_is_name
-    @name
-  end
-
-  def change_my_name=(new_name)
-    @name = new_name
-  end
-
-  def what_is_occupation
-    @occupation
-  end
-
-  def change_my_occupation=(new_occupation)
-    @occupation = new_occupation
-  end
-
+  
+## If the method name of `name` is different, than we can't write it in shortcut of attr_reader:::::::::::::::::
+#   def name
+#       @name
+#   end
 
 end
+
+
+class Greetings
+  def initialize
+    @name = NameData.new
+  end
+
+  def hello
+    puts "Hello #{@name.name}! How wonderful to see you today!"
+  end
+
+end
+
+greet = Greetings.new
+greet.hello
