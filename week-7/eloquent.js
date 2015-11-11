bc89dbcc910e18a479c22c6b5d393c4ac1de45d0
Fatma Ocal <contactfatma@gmail.com>
2015-11-10 22:16:46 -0800

    Update release 1

---
+++ week-7/eloquent.js
@@ -0,0 +1,62 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+
+
+
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+
+
+
+// Functions
+
+// Complete the `minimum` exercise.
+
+
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+// REFLECTIONS:
+
+// Release 1:
+// Introduction
+// Did you learn anything new about JavaScript or programming in general?
+  //  Programming or Javascript requires less code to do the same operation comparing to the old days coding when there was no programming languages. The current programming logic is less complex and more friendly to programmers. With help of blocks constructed in while, for or if statements, there is no need to jump back or forth within the code, while makes it easier to loop for the block. Web browsers are not the only platforms on which Javascript us used. Some databases, such as MongoDB and CouchDB, use Javascript as their scripting and query language. While I was reading throgh Eloquent Javascript book, I found out that the website enables you to edit and run the code, which was great to see different outputs for different input right away.
+
+// Are there any concepts you feel uncomfortable with?
+// Nope.
+
+//Ch.1 Values, Types and Operators
+//Identify two similarities and two differences between JavaScript and Ruby syntax with regard to numbers, arithmetic, strings, booleans, and various operators.
+  // Values in Javascript is the chunk of the bits that were separated and each represent pieces of information. There are six basic types of values in JavaScript: numbers, strings, Booleans, objects, functions, and undefined values.
+  //The following values are the same in both Javascript and Ruby.
+    // 1. Numbers are the numerical values, writen as 10. Numbers vary from whole number, integer or frctional numbers, float.
+    //2. Strings are the basic data types that reqpresent the text. They are written in both Ruby and Javascript between quotes. While we are writing strings, whenever a backslash(\) is found inside quoted text, it indicates that the character after ita has a special meaning, which is called escaping the character. When 'n' character comes after backslash, it interpretes as new line (\n) which is also the same in Ruby. Besides `\n`, Javascript has the same `+` operator that concatenates two strings.
+    //3. Booleans are the true and false states.
+    //4. Objects are
+
+
+//Ch.2 Program Structure
+What is an expression?
+What is the purpose of semicolons in JavaScript? Are they always required?
+What causes a variable to return undefined?
+Write your own variable and do something to it in the eloquent.js file.
+What does console.log do and when would you use it? What Ruby method(s) is this similar to?
+Write a short program that asks for a user to input their favorite food. After they hit return, have the program respond with "Hey! That's my favorite too!" (You will probably need to run this in the Chrome console (Links to an external site.) rather than node since node does not support prompt or alert). Paste your program into the eloquent.js file.
+Describe while and for loops
+What other similarities or differences between Ruby and JavaScript did you notice in this section?
+Complete at least one of the exercises (Looping a Triangle, FizzBuzz, of Chess Board) in the eloquent.js file.
+
+//Ch3. FunctionsWhat are the differences between local and global variables in JavaScript?
+When should you use functions?
+What is a function declaration?
+Complete the minimum exercise in the eloquent.js file.
+
+//Ch.4
+Skip the sections on the Lycanthrope's log, Computing Correlations, and sections from Further Arrayology to the Global Object, but read the chapter summary.
+What is the difference between using a dot and a bracket to look up a property? Ex.
