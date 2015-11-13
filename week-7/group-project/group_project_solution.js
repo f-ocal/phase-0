0fe3f75b3b4f52130d16f7d50cf2ce6c0eb45a74
Fatma Ocal <contactfatma@gmail.com>
2015-11-12 22:45:26 -0800

    Update eloquent

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,33 @@
+
+Person 1 : Fatma
+
+User Story:
+
+As a user, I want to do build a function that takes an input from the user for an nested array and a hash with key/value pairs.
+The input for nested array should be composed of two arrays.
+I want one of the array takes odd integers and the array takes even integers from the user.
+I want a function that gives the sum of the odd array and the sum of the even array and give me the total sum of the two arrays.
+It should also be more smart and give me the sum of all elements in an array with an odd length.
+It should also be more smart and give me the sum of all elements in an array with an even length.
+Then, I want this program to give me the length of all three arrays also give their minimum and maximum values.
+
+I want another two more functions one for mean operation and the other is for median operation.
+The first function should return the mean of the most outer array (has two arrays in it).
+I want the same function that gives mean of all elements with an odd length and an even length.
+The second function should return the median of the most outer array (has two arrays in it).
+I want the same function that gives the median of all elements with an odd length and an even length.
+
+Once user finishes entering information about arrays, I want the user key in his/her information about name, age, occupation and his telephone number into another function.
+if he is younger than 25 years and student, my program should wish him/her Good luck in her exams.
+if he is older than 25 years and he is a programmer, my program should tell him/her Happy Coding and Keep Coding!.
+No matter what is his age if he doesnt have a job, my program should tell him to start looking for jobs. Anything else he enters for his occupation, dont wish him anything.
+I want his/her telephone number same value as every elements of the most outer array (that has both even and odd numbers).
+If his telephone number is more or less than 11 digits, tell him the number is wrong information. If it is 11 digits, program should say Thanks, Mate!. Finally, print out all the information as summary he gave to the program as input.
+
+Person 2 : Shea
+Pseudocode:
+
+
+Person 3: Fatma
+
+Person 4: Shea
