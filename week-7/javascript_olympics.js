7d3569d3187dfb2c2600eea7aac47e2857820a4d
Fatma Ocal <contactfatma@gmail.com>
2015-11-14 11:14:04 -0800

    Include javascript challenge

--- week-7/javascript_olympics.js
+++ week-7/javascript_olympics.js
@@ -1,37 +1,66 @@
 // JavaScript Olympics
 
-// I paired [by myself, with:] on this challenge.
+// I paired with Tal on this challenge.
 
-// This challenge took me [#] hours.
+// This challenge took us 1 hour.
 
+// Release 1: Bulk Up
 
-// Warm Up
+var athlete1 = {name: "Sarah Hughes", event: "Ladies Singles"}
+var athlete2 = {name: "Bob Hughes", event: "Mens Singles"}
+var athletes = [athlete1, athlete2]
 
+function eventWon(athletes) {
+ for (var i=0; i < athletes.length; i++) {
+   athletes[i].win = athletes[i].name + " won the event " + athletes[i].event;
+   console.log(athletes[i].win);
 
-
-
-// Bulk Up
-
-
-
-
-// Jumble your words
-
-
-
-
-// 2,4,6,8
+ }
+}
+eventWon(athletes)
 
 
+// Release 2: Jumble your words
+
+var string = "StringToReverse"
+function reverseString(string){
+ console.log(string.split("").reverse().join(""));
+}
+reverseString(string)
 
 
-// "We built this city"
+// Release 3: 2, 4, 6 ,8! Who do we appreciate?
+
+var evenArray =[]
+function evenNumbers(array){
+for (var i=0; i < array.length; i++){
+ if(array[i] % 2 == 0){
+   evenArray.push(array[i])
+   }
+ }
+ console.log(evenArray)
+}
+evenNumbers([1,2,3,4,5,6])
 
 
+// Release 4: "We Built this City": Build a constructor function
 
+function Athlete(name, age, sport, quote){
+ this.name = name;
+ this.age = age;
+ this.sport = sport;
+ this.quote = quote;
+}
+
+var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
+console.log(michaelPhelps.constructor === Athlete)
+console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
 
 // Reflection
 /*
 What JavaScript knowledge did you solidify in this challenge?
 What are constructor functions?
 How are constructors different from Ruby classes (in your research)?
+
+
+*/
