1f00e4ec0ed2a05d89e2c889ecd7012b1600b11a
Fatma Ocal <contactfatma@gmail.com>
2015-11-16 12:59:41 -0800

    Update trial file

---
+++ week-6/trial/trial.js
@@ -0,0 +1,29 @@
+// var symbols = ["flag", "postage stamp", "logo", "signature", "seal"]
+//
+// var counter = 3
+//
+//
+//
+// console.log(symbols[counter]);
+//
+// console.log(symbols.3);
+
+function add(num1, num2) {
+  return num1 + num2
+}
+
+console.log(add(1, 2, 3));
+var numberSample = [5, 2, 3, 1, 4]
+console.log(numberSample.sort());
+var answers = new Object();
+// console.log(typeof)
+console.log(typeof answers);
+
+var answerBank = {
+  optionOne: "Wrong!",
+  optionTwo: "Wrong!",
+  optionThree: "Correct",
+};
+console.log(answerBank. optionThree);
+console.log(answerBank. optionThree);
+console.log(answerBank["optionThree"]);
