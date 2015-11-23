01de0f30805d1de910d1120a4a53937d63f9a00e
Fatma Ocal <contactfatma@gmail.com>
2015-11-23 02:24:37 -0800

    Include rspec

---
+++ week-9/project/book_spec.rb
@@ -0,0 +1,42 @@
+require_relative 'book.rb'
+
+describe  Book do
+
+  before :each do
+      @book = Book.new "Title", "Author", :category
+  end
+
+  describe "#new" do
+      it "returns a new book object" do
+        @book.should(be_an_instance_of(Book))
+      end
+
+      it "throws an ArgumentError when given fewer than 3 parameters" do
+        lambda {Book.new "Title", "Author"}.should raise_exception(ArgumentError)
+      end
+  end
+
+  describe "#title" do
+    it "returns the correct title" do
+      @book.title.should eql "Title"
+    end
+  end
+
+  describe "#author" do
+    it "returns the correct author" do
+      @book.author.should eql "Author"
+    end
+  end
+
+    describe "#category" do
+      it "returns the correct category" do
+        @book.category.should eql :category
+      end
+    end
+
+  it "accepts new books" do
+    @book.add_book(Book.new("Designing for the Web", "Mark Boulton", :design))
+    @book.get_book("Designing for the Web").should be_an_instance_of(Book)
+  end
+
+end
