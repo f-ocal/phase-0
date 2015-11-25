32c1f97e79052c952e7b41bb05fe494b9e72cd7d
Fatma Ocal <contactfatma@gmail.com>
2015-11-24 16:08:22 -0800

    Include car code

--- week-9/ruby-review-1/ruby_carClass.rb
+++ week-9/ruby-review-1/ruby_carClass.rb
@@ -28,7 +28,7 @@
 # Define drop_off method for delivering the pizza, take it from our array once it is delivered.
 # Display whatever left from the pizza box.
 
-# 3. Initial Solution
+# # 3. Initial Solution
 class Car
 
   def initialize(model, color)
@@ -38,6 +38,7 @@ class Car
     @last_action = "You haven't started driving yet."
     @total_distance = 0
     puts "You are driving #{@model}, #{@color} car."
+    @pizza_box = []
   end
 
   def drive(distance, speed)
@@ -63,18 +64,129 @@ class Car
     self.display
   end
 
+  def pick_up(pizza_name)
+    @pizza_box.push(pizza_name)
+  end
+
+  def drop_off
+    puts "You just dropped off #{@pizza_box[0]}."
+    @pizza_box.shift
+    puts "The next pizza to be delivered is #{@pizza_box[-1]}."
+  end
+
 
   def display
     puts "Your current speed is #{@speed} mph."
     puts "You have traveled total of #{@total_distance} miles."
     puts @last_action
+    # puts "The pizza box has #{@pizza_box} left."
+
   end
 
 
 end
 
+class Pizza
+
+  def initialize(toppings, size, crust)
+    @toppings = toppings
+    @size = size
+    @crust = crust
+  end
+
+  def description
+    puts "Your pizza is a #{@size} inch pizza with #{@crust} crust and #{@toppings}."
+  end
+
+end
+
+
+# 4. Refactored Solution
+#removed variables fir speed equal to zero at initialize.
+#cleaned up text printouts to be more logical.
+
+class Car
+
+  def initialize(model, color)
+    @model = model
+    @color = color
+    @total_distance = 0
+    @last_action = "You haven't started driving yet."
+    puts "You are driving #{@model}, #{@color} car."
+    @pizza_box = []
+  end
+
+  def drive(distance, speed)
+    @total_distance += distance
+    @speed = speed
+    @last_action = "You were driving #{distance} miles at #{speed} mph."
+  end
+
+  def change_speed(speed)
+    @speed = speed
+  end
+
+  def turn_right
+    @last_action = 'You just made a right turn.'
+  end
+
+  def turn_left
+    @last_action= 'You just made a left turn.'
+  end
+
+  def stop
+    @speed = 0
+    self.display
+  end
+
+  def pick_up(pizza_name)
+    @pizza_box.push(pizza_name)
+  end
+
+  def drop_off
+    puts "You just dropped off #{@pizza_box[0]}."
+    @pizza_box.shift
+    puts "The next pizza to be delivered is #{@pizza_box[-1]}."
+  end
+
+
+  def display
+    puts "Your current speed is #{@speed} mph."
+    puts "You have traveled total of #{@total_distance} miles."
+    puts @last_action
+
+  end
+
+
+end
+
+class Pizza
+
+  def initialize(toppings, size, crust)
+    @toppings = toppings
+    @size = size
+    @crust = crust
+  end
+
+  def description
+    puts "Your pizza is a #{@size} inch pizza with #{@crust} crust and #{@toppings}."
+  end
+
+end
+
+
+
+# 1. DRIVER TESTS GO BELOW THIS LINE
+
 #DRIVER CODE
 new_car = Car.new('Tesla Model S','red')
+pepperoni = Pizza.new('pepperoni',16,'pan')
+pepperoni.description
+veggie = Pizza.new('veggie',14,'thick')
+veggie.description
+new_car.pick_up('pepperoni')
+new_car.pick_up('veggie')
+new_car.display
 new_car.drive(0.25,25)
 new_car.stop
 new_car.turn_right
@@ -85,21 +197,10 @@ new_car.drive(0.25,15) #if we have time; lets confirm how to make second argumen
 new_car.stop
 new_car.turn_left
 new_car.drive(1.4,35)
+new_car.drop_off
 new_car.stop
 
 
-
-
-
-
-# 4. Refactored Solution
-
-
-
-
-
-
-# 1. DRIVER TESTS GO BELOW THIS LINE
 
 =begin
 Create a new car of your desired model and type
